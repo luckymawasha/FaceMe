ec28d1ce4c22caea709f9cf0b62b676958730259
luckymawasha <luckymawasha72@gmail.com>
2025-12-16 10:22:04 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/authStore";
+import { useAuthStore } from "./zustand/authStore";
 import AuthPage from "./components/auth/AuthPage";
 import FeedPage from "./pages/FeedPage";
 import WatchPage from "./pages/WatchPage";
